52239603ff74223ede4121427823368bdb79b61a
Igor <igorchesalin@gmail.com>
2020-11-01 19:21:03 +0300

    б4, д3, б10, д5, д30, б17, д17, д27

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@ closelink.addEventListener("click", function (event) {
 });
 
 
-
-
+// var link = document.querySelector(".search-btn");
 // var popup = document.querySelector(".modal-content");
+
+// link.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   popup.classList.toggle("modal-content-show");
+// });
