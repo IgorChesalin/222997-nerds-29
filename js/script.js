2696b5756a040afa7a8df750aacb8bb7a45c824f
Igor <igorchesalin@gmail.com>
2020-11-02 21:12:01 +0300

    д20 кнопки ссылками, разное

--- js/script.js
+++ js/script.js
@@ -13,11 +13,3 @@ closelink.addEventListener("click", function (event) {
   popup.classList.remove("modal-content-show");
 });
 
-
-// var link = document.querySelector(".search-btn");
-// var popup = document.querySelector(".modal-content");
-
-// link.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   popup.classList.toggle("modal-content-show");
-// });
