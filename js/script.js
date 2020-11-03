6f167b54033f96f2145d0b32bbdffe6fb6ce2ae6
Igor <igorchesalin@gmail.com>
2020-11-03 09:39:35 +0300

    шейк, минификация

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ closelink.addEventListener("click", function (event) {
   popup.classList.remove("modal-content-show");
 });
 
+// Ошибка ввода
+var submitBtn = document.querySelector(".form-button-send");
+var customerName = popup.querySelector(".customer-name");
+var customerEmail = popup.querySelector(".customer-email");
+var customerMessage = popup.querySelector(".customer-text")
+
+submitBtn.addEventListener("click", function (evt) {
+  if (!customerName.value || !customerEmail.value || !customerMessage.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
